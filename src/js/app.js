afdb7653b8b17e2c5c2dbc3d4d48c0e812080e1c
bdawie <ibrahimbdawie@gmail.com>
2018-03-11 21:57:21 +0200

    first commit bebel-scss

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+
+const randomInteger = document.querySelector('#randomInteger');
+const randomRange = document.querySelector('#randomRange');
+const outputParagraph = document.querySelector('#outputParagraph');
+
+randomInteger.addEventListener('click',outputRandomInt);
+randomRange.addEventListener('click',outputRandomRange);
+
+function outputRandomInt(){
+    outputParagraph.textContent = RandomGenerator.randomInteger();
+}
+
+function outputRandomRange(){
+    outputParagraph.textContent = RandomGenerator.randomRange(1,500);
+}
